3adf3ffada248eba5792a9d3deb8c899513be220
Mike Leach <nderzero@gmail.com>
2013-06-02 18:38:32 -0500

    Preparing to upload to Heroku. Added Procfile and set footnotr db to Postgres. -for real this time

--- footnotrserver/footnotrserver/settings.py
+++ footnotrserver/footnotrserver/settings.py
@@ -145,6 +145,13 @@ REST_FRAMEWORK = {
     
 }
 
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
