2a2ea37e6f77fea371cabdea8056b56297b5f377
Mike Leach <nderzero@gmail.com>
2013-06-02 21:34:00 -0500

    Converted database to postgres. synced db and (fake)migrated for south.

--- footnotrserver/footnotrserver/settings.py
+++ footnotrserver/footnotrserver/settings.py
@@ -16,10 +16,10 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',#'footnotrdb',                      # Or path to database file if using sqlite3.
+        'NAME': 'footnotr',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'cxmszzwlwqejwz',
-        'PASSWORD': 'fDnEiEiOQ7F73sFFc41b8R2anR',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
