097e63262f85f95cfc5864e6eee45cec4b7f1d8f
Mike Leach <nderzero@gmail.com>
2013-06-02 22:37:09 -0500

    Fixing article creator migration

--- footnotrserver/articles/migrations/0002_auto__add_field_article_creator.py
+++ footnotrserver/articles/migrations/0002_auto__add_field_article_creator.py
@@ -8,55 +8,15 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        # Adding model 'Article'
-        db.create_table(u'articles_article', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('guid', self.gf('django.db.models.fields.CharField')(unique=True, max_length=32)),
-        ))
-        db.send_create_signal(u'articles', ['Article'])
-
-        # Adding model 'Annotation'
-        db.create_table(u'articles_annotation', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('article', self.gf('django.db.models.fields.related.ForeignKey')(related_name='annots', to=orm['articles.Article'])),
-            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'])),
-            ('pdfLibID', self.gf('django.db.models.fields.IntegerField')()),
-            ('xml', self.gf('django.db.models.fields.TextField')()),
-        ))
-        db.send_create_signal(u'articles', ['Annotation'])
-
-        # Adding model 'Comment'
-        db.create_table(u'articles_comment', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('annotation', self.gf('django.db.models.fields.related.ForeignKey')(related_name='comments', to=orm['articles.Annotation'])),
-            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'])),
-            ('comment', self.gf('django.db.models.fields.TextField')()),
-            ('created', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
-        ))
-        db.send_create_signal(u'articles', ['Comment'])
-
-        # Adding model 'Vote'
-        db.create_table(u'articles_vote', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('comment', self.gf('django.db.models.fields.related.ForeignKey')(related_name='votes', to=orm['articles.Comment'])),
-            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'])),
-        ))
-        db.send_create_signal(u'articles', ['Vote'])
+        # Adding field 'Article.creator'
+        db.add_column(u'articles_article', 'creator',
+                      self.gf('django.db.models.fields.related.ForeignKey')(default=2, related_name='articles', to=orm['auth.User']),
+                      keep_default=False)
 
 
     def backwards(self, orm):
-        # Deleting model 'Article'
-        db.delete_table(u'articles_article')
-
-        # Deleting model 'Annotation'
-        db.delete_table(u'articles_annotation')
-
-        # Deleting model 'Comment'
-        db.delete_table(u'articles_comment')
-
-        # Deleting model 'Vote'
-        db.delete_table(u'articles_vote')
+        # Deleting field 'Article.creator'
+        db.delete_column(u'articles_article', 'creator_id')
 
 
     models = {
@@ -70,6 +30,7 @@ class Migration(SchemaMigration):
         },
         u'articles.article': {
             'Meta': {'object_name': 'Article'},
+            'creator': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'articles'", 'to': u"orm['auth.User']"}),
             'guid': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '32'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'title': ('django.db.models.fields.CharField', [], {'max_length': '255'})
